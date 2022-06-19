b6376e23295b6fb7d7f92db29df03dcfd1f5a6c1
raushaniit2020 <85079323+raushaniit2020@users.noreply.github.com>
2022-06-19 16:11:18 +0530

    Add files via upload

---
+++ newscafe/newsvibes/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import SignUpView
+from . import views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path('search', views.search, name='search'),
+    
+    ]
+    # path('category', views.category, name='category')
